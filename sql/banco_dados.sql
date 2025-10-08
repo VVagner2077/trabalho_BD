03fe208ef75c3051b2c50e01c418f1fa24cdbb6a
Wagner <wagnerdossantos2311@gmail.com>
2025-10-08 20:11:37 -0300

    arquivos zips removido na main

---
+++ sql/banco_dados.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Clientes (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    telefone VARCHAR(11) NOT NULL,
+    cpf VARCHAR(14) NOT NULL
+);
+
+CREATE TABLE Betoneiras (
+    id_betoneira INT AUTO_INCREMENT PRIMARY KEY,
+    modelo VARCHAR(100) NOT NULL,
+    status boolean NOT NULL,
+    valor DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE Alugueis (
+    id_aluguel INT AUTO_INCREMENT PRIMARY KEY,
+    id_cliente INT NOT NULL,
+    id_betoneira INT NOT NULL,
+    data_inicio DATE NOT NULL,
+    data_devolucao DATE NOT NULL,
+    FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente),
+    FOREIGN KEY (id_betoneira) REFERENCES Betoneiras(id_betoneira)
+);
