9e2ad46d26e0e55e03d7f512330c2941ae87bc83
unknown <wagnerdossantos2311@gmail.com>
2025-10-19 13:58:34 -0300

    modificação do nome da pasta relatorio para pesquisa

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-# main.py
-
-# Importa a função principal 'menu' do módulo 'menu.py' que está dentro da pasta 'utils'
 from utils.menu import menu
 
 def main():
-    """
-    Função principal que inicia a aplicação.
-    """
-    # Chama a função que exibe o menu principal e inicia o loop da aplicação
+
     menu()
 
 if __name__ == "__main__":
-    # Esta verificação garante que a função main() só será executada
-    # quando este arquivo (main.py) for rodado diretamente.
+
     main()
 
     
